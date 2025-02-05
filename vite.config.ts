0f9674797bb1ef645133b7b8e0e7833cb77da958
AmanCHM <aman.kumar@chicmicstudios.in>
2025-02-05 18:56:36 +0530

    Fix: vite coniguration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, UserConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
+// Define and export Vite configuration
+const config: UserConfig = defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 2000, // Set chunk size limit to 2MB
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Alias for cleaner imports
+    },
+  },
+});
+
+export default config;
